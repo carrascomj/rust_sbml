b224f0e21959b1fb4a799db41ee3be395c2c0d3d
carrascomj <carrascomurielj@gmail.com>
2020-11-13 17:56:48 +0100

    feat: write ModelRaw :sparkles:

--- src/list_of.rs
+++ src/list_of.rs
@@ -1,52 +1,52 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use super::base_types::{
     Compartment, Constraint, InitialAssignment, Objective, Parameter, Reaction, Species,
     UnitDefinition,
 };
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfUnitDefinitions {
     #[serde(rename = "unitDefinition", default)]
     pub unit_definitions: Vec<UnitDefinition>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfCompartments {
     #[serde(rename = "compartment", default)]
     pub compartments: Vec<Compartment>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfSpecies {
     pub species: Vec<Species>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfReactions {
     #[serde(rename = "reaction", default)]
     pub reactions: Vec<Reaction>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfParameters {
     #[serde(rename = "parameter", default)]
     pub parameters: Vec<Parameter>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfInitialAssignments {
     #[serde(rename = "initialAssigment", default)]
     pub initial_assignments: Vec<InitialAssignment>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfConstraints {
     #[serde(rename = "constraints", default)]
     pub constraints: Vec<Constraint>,
 }
 
-#[derive(Deserialize, PartialEq, Debug, Default)]
+#[derive(Deserialize, Serialize, PartialEq, Debug, Default, Clone)]
 pub struct ListOfObjectives {
     #[serde(rename = "objective", default)]
     pub objectives: Vec<Objective>,
