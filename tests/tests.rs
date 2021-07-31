7c88789f6ab881ed03f98fc9fd5225206ad2ba96
carrascomj <carrascomurielj@gmail.com>
2021-07-31 14:46:12 +0200

    test: test for constraints now that MathML is parsed

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,7 @@
-use rust_sbml::{parse_document, Model, ModelRaw, ModelUnits, UnitSId, UnitSIdRef};
+use rust_sbml::{
+    mathml, mathml::Math, mathml::MathNode, parse_document, Constraint, Message, Model, ModelRaw,
+    ModelUnits, UnitSId, UnitSIdRef,
+};
 
 #[test]
 fn read_units_succeed() {
@@ -51,4 +54,65 @@ fn read_abstraction_objective_succeeds() {
             .unwrap(),
         "R_BIOMASS_Ecoli_core_w_GAM"
     );
+}
+
+#[test]
+fn test_constraints() {
+    let example = include_str!("test_constraint.xml");
+    let res = parse_document(example).unwrap();
+    let expect = Model {
+        model_units: ModelUnits {
+            time_units: Some(UnitSIdRef::SIUnit(UnitSId::second)),
+            substance_units: Some(UnitSIdRef::SIUnit(UnitSId::mole)),
+            extent_units: Some(UnitSIdRef::SIUnit(UnitSId::mole)),
+            volume_units: None,
+            area_units: None,
+            length_units: None,
+            conversion_factor: None,
+        },
+
+        constraints: vec![Constraint {
+            math: Some(Math {
+                content: MathNode::apply(vec![
+                    MathNode::And,
+                    MathNode::apply(vec![
+                        MathNode::Lt,
+                        MathNode::Cn(mathml::Cn {
+                            cn_type: mathml::NumberType::Real,
+                            content: String::from("1"),
+                            base: mathml::Base::default(),
+                            definition_url: None,
+                            encoding: None,
+                            unit: Some(UnitSIdRef::SIUnit(UnitSId::mole)),
+                        }),
+                        MathNode::Ci {
+                            content: "S1".to_owned(),
+                            ci_type: None,
+                        },
+                    ]),
+                    MathNode::apply(vec![
+                        MathNode::Lt,
+                        MathNode::Ci {
+                            content: "S1".to_owned(),
+                            ci_type: None,
+                        },
+                        MathNode::Cn(mathml::Cn {
+                            cn_type: mathml::NumberType::Real,
+                            content: String::from("100"),
+                            base: mathml::Base::default(),
+                            definition_url: None,
+                            encoding: None,
+                            unit: Some(UnitSIdRef::SIUnit(UnitSId::mole)),
+                        }),
+                    ]),
+                ]),
+            }),
+            message: Some(Message {
+                content: "Species S1 is out of range.".to_owned(),
+            }),
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+    assert_eq!(res, expect);
 }
